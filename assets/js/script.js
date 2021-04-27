dd12e0f2b6b50d74302f1e1ca31838295b535676
Abigail <asexton5@nd.edu>
2021-04-27 18:35:59 -0400

    added in display name function

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,32 @@ var searchHistoryItems = document.querySelector("#search-history-items");
 var currentWeatherCity = document.querySelector("#current-weather-city");
 var currentWeatherData = document.querySelector("#current-weather");
 var forecastElement = document.querySelector("#forecast");
+
+// define variables
+var displayName;
+var searchTerms = [];
+var searchHistory = [];
+
+var defineDisplayName = function(location) {
+    /* display the location as city, state, country */
+
+    // define the location components
+    var city = location.adminArea5;
+    var state = location.adminArea3;
+    var country = location.adminArea1;
+
+    // construct an array of the location components
+    var tempDisplayName = [];
+    if (city) {
+        tempDisplayName.push(city);
+    }
+    if (state) {
+        tempDisplayName.push(state);
+    }
+    if (country) {
+        tempDisplayName.push(country);
+    }
+
+    // return the joined array so that we don't need to deal with extra commas
+    return tempDisplayName.join(", ");
+}
